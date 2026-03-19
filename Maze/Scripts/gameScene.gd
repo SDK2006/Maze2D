a4c5747bdc2e5d38085d6dcd195f31ad12aecdc2
SDK2006 <92155641+SDK2006@users.noreply.github.com>
2026-03-19 19:58:07 +0530

    SDK2
    made the code smoother, added theseus functionality and tried unsuccessfully adding multiplayer functionality

--- Maze/Scripts/gameScene.gd
+++ Maze/Scripts/gameScene.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
+@onready var MPlayer = load("res://Maze/Scenes/Minotaur.tscn").instantiate()
+@onready var TPlayer = load("res://Maze/Scenes/Theseus.tscn").instantiate()
+
+
 func _ready() -> void:
 	$FadeTransition/AnimationPlayer.play("fade_out")
 	var generator = get_node("MazeGenerator")
 	GameState.coords = Vector2(generator._mazeWidth*20-20, generator._mazeDepth*20-20)
-	if GameState.playerSelection=="Minotaur":
-		var MPlayer = load("res://Maze/Scenes/Minotaur.tscn").instantiate()
+	
+	if GameState.playerSelection == "Minotaur":
 		add_child(MPlayer)
-	if GameState.playerSelection=="Theseus":
-		var TPlayer = load("res://Maze/Scenes/Theseus.tscn").instantiate()
+	elif GameState.playerSelection == "Theseus":
 		add_child(TPlayer)
-	print(GameState.coords)
+
+func _process(_delta):
+	var l = multiplayer.get_peers()
+	print(l)
